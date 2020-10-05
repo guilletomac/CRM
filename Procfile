4b34268ba598e7f362716c407080946132ee6539
Guille <guillermotomac@gmail.com>
2020-10-05 19:19:10 +0100

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn customer_management.wsgi --log-file - 
---
+++ accounts/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User, Group
+from .models import Customer
+
+def customer_profile(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name='customer')
+        
+        instance.groups.add(group)
+
+        Customer.objects.create(
+            user=instance,
+            name=instance.username,
+        )
+
+post_save.connect(customer_profile, sender=User)
